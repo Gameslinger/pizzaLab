d9921dc40cb47034d6d4c6afda4cf1205019fb1f
EAusman <emma.ausman@gmail.com>
2021-11-03 11:03:02 -0600

    I maybe fixed it

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Pizza logo" src="../assets/img/logo.png" />
+    <HelloWorld msg="Welcome to Tastee Pizza" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+<style>
+.home{
+background:#DAF5FF;
+font-size:30pt;
+color:#587291;
+}
+</style>
