344932ff586fc082663deb1e8c6c529740e95012
Gameslinger <gabe2max@gmail.com>
2021-11-05 02:17:10 +0100

    Sadness

--- src/toppings.js
+++ src/toppings.js
@@ -1,49 +1,49 @@
 let toppingsMock = [{
     id: 1,
     name:"Canadian Bacon",
-    price: "$2.37",
+    price: 2.37,
     image: "canadianBacon.png"
 },
     {
 	id: 2,
 	name: "Chicken",
-	price: "$1.14",
+	price: 1.14,
 	image: "chicken.png"
     },
     {
 	id: 3,
 	name: "Mushrooms",
-	price: "$0.75",
+	price: 0.75,
 	image: "mushrooms.png"
     },
     {
 	id: 4,
 	name: "Olives",
-	price: "$1.20",
+	price: 1.20,
 	image: "olives.png"
     },
     {
 	id: 5,
 	name: "Peppers",
-	price: "$0.01",
+	price: 0.01,
 	image: "peppers.png"
     },
     {
 	id: 6,
 	name: "Pineapple",
-	price: "$3.33",
+	price: 3.33,
 	image: "pineapple.png",
     },
     {
 	id: 7,
 	name: "Sausage",
-	price: "$2.99",
+	price: 2.99,
 	image: "sausage.png"
     },
     {
 	id: 8,
 	name: "Pepperoni",
-	price: "$2.55",
+	price: 2.55,
 	image: "pepperoni.png"
     },
 ]
