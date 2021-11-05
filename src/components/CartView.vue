d7c31b7ed0c7dbd9263f39e830531ebedd98ffdd
EAusman <emma.ausman@gmail.com>
2021-11-04 18:57:23 -0600

    gabe made a new logo

---
+++ src/components/CartView.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+export default {
+	name: "CartView",
+}
+</script>	
+
+<style scoped>
+.toppings .topping{
+display:flex;
+justify-content:center;
+}
+
+.topping img {
+position: absolute;
+
+}
+</style>
---
+++ src/views/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Cart">
+	<CartView/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CartView from '@/components/CartView.vue'
+
+export default {
+  name: 'Cart',
+  components: {
+    CartView
+  },
+  data(){
+},	
+}
+</script>
